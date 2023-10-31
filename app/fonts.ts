9a6ea211202e428c2596ca13e8dd529dc1d8a643
dks-customs <dominik.kaczmarczyk961@gmail.com>
2023-10-31 09:28:20 +0100

    changed fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,54 +1,9 @@
 import {
   Playfair_Display,
   Fira_Sans,
+  Playfair,
+  Noto_Sans,
   Noto_Serif,
-  Open_Sans,
-  EB_Garamond,
-  Lato,
-  Play,
-  Barlow_Semi_Condensed,
-  Roboto_Condensed,
-  Poltawski_Nowy,
-  Cardo,
-  Fira_Sans_Condensed,
-  Fira_Sans_Extra_Condensed,
-  Oswald,
-  Anton,
-  Lusitana,
-  Fira_Code,
-  Nanum_Gothic,
-  Inter,
-  Merriweather,
-  Lora,
-  PT_Serif,
-  Source_Serif_4,
-  Frank_Ruhl_Libre,
-  Tinos,
-  Spectral,
-  Alice,
-  Prata,
-  Unna,
-  Old_Standard_TT,
-  Crimson_Pro,
-  Roboto_Serif,
-  Vidaloka,
-  Adamina,
-  Rufina,
-  Petrona,
-  GFS_Didot,
-  Yrsa,
-  Cantata_One,
-  Kaisei_Opti,
-  Rasa,
-  Hahmlet,
-  Cambo,
-  Ledger,
-  Benne,
-  Montaga,
-  Hina_Mincho,
-  Sumana,
-  Bebas_Neue,
-  Kanit,
 } from "next/font/google";
 
 // export const displayFont = Playfair_Display({
@@ -62,12 +17,17 @@ import {
 //Petrona !!!
 //Cantata_One !!!!
 
-export const displayFont = Noto_Serif({
+export const displayFont = Playfair_Display({
   subsets: ["latin"],
   variable: "--font-display",
 });
 
-export const uiFont = Lato({
+export const bodyFont = Noto_Serif({
+  subsets: ["latin"],
+  variable: "--font-body",
+});
+
+export const uiFont = Fira_Sans({
   weight: ["300", "400", "700"],
   subsets: ["latin"],
   variable: "--font-ui",
